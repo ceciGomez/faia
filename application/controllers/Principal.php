fed600bfa585e138874e687d9c63a7c6e73b35b5
ceciliadrunk <cecilia.mlgz@gmail.com>
2016-04-25 18:22:46 -0300

    Proceso de entrada de datos

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -10,6 +10,37 @@ class Principal extends CI_Controller {
 	public function ingresar(){
 		$this->load->view('ingresarDatos');
 	}
+	public function ingresarDatos(){
+		$operando1 = $this->input->post('op1');
+		$operando2 = $this->input->post('op2');
+		$resultado = $this->input->post('res');
+
+		$operando1 = str_split($operando1);
+		$operando1 = array_reverse($operando1);
+		$operando2 = str_split($operando2);
+		$operando2 = array_reverse($operando2);
+		$resultado = str_split($resultado);
+		$resultado = array_reverse($resultado);
+
+		$vectorInicio = array_merge($resultado,$operando2,$operando1);
+		$vectorInicio = array_unique($vectorInicio);
+		var_dump($vectorInicio);
+
+	}
+	public function crearMatrizInicial()
+	{
+	
+        $vector1 = array('0','1', '2','3','4','5','6','7','8','9');
+        $vector2 = array('0','1', '2','3','4','5','6','7','8','9');
+        $vector3 = array('0','1', '2','3','4','5','6','7','8','9');
+        $vector4 = array('0','1', '2','3','4','5','6','7','8','9');
+        $vector5 = array('0','1', '2','3','4','5','6','7','8','9');
+        shuffle($vector1);
+        shuffle($vector2);
+        shuffle($vector3);
+        shuffle($vector4);
+        shuffle($vector5);
+	}
 
 }
 
--- application/views/ingresarDatos.php
+++ application/views/ingresarDatos.php
@@ -5,10 +5,10 @@
       <title> IA</title>
    </head>
    <body>
-		<form form-horizontal action="ingresarDatos_submit" method="POST" accept-charset="utf-8">
-			<label>Palabra 1:</label><input>
-			<label>Palabra 2:</label><input>
-			<label>Resultado:</label><input>
+		<form action="ingresarDatos" method="POST" accept-charset="utf-8">
+			<label>Palabra 1:</label><input name="op1" >
+			<label>Palabra 2:</label><input name="op2">
+			<label>Resultado:</label><input name="res">
 			<button type="submit">resolver</button>
 		</form>
 	</body>
