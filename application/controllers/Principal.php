ea6fc2b3d8d0c50b227ec8a45ee2eb8ea8885ff9
ceciliadrunk <cecilia.mlgz@gmail.com>
2016-04-25 17:49:11 -0300

    Vista de ingreso de datos

---
+++ application/controllers/Principal.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Principal extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+	public function ingresar(){
+		$this->load->view('ingresarDatos');
+	}
+
+}
+
+/* End of file CI_Principal.php */
+/* Location: ./application/controllers/CI_Principal.php */
---
+++ application/views/ingresarDatos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8" />
+      <title> IA</title>
+   </head>
+   <body>
+		<form form-horizontal action="ingresarDatos_submit" method="POST" accept-charset="utf-8">
+			<label>Palabra 1:</label><input>
+			<label>Palabra 2:</label><input>
+			<label>Resultado:</label><input>
+			<button type="submit">resolver</button>
+		</form>
+	</body>
+	</html>
