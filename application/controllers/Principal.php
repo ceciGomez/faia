8d052631896fa66f6665a6090be9a7702d6d1cb1
ceciliadrunk <cecilia.mlgz@gmail.com>
2016-05-06 19:38:05 -0300

    extraccion de valores - intento 2

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -19,6 +19,8 @@ class Principal extends CI_Controller {
 		$operando2 = $this->input->post('op2');
 		$resultado = $this->input->post('res');
 
+		//la funcion str_split divide en elementos y lo pone en un arreglo
+		//la funcion array_reverse da vuelta el array
 		$operando1 = str_split($operando1);
 		$operando1 = array_reverse($operando1);
 		$operando2 = str_split($operando2);
@@ -27,7 +29,9 @@ class Principal extends CI_Controller {
 		$resultado = array_reverse($resultado);
 
 		$vectorInicio = array_merge($resultado,$operando2,$operando1);
+		//Elimna los elementos repetidos
 		$vectorInicio = array_unique($vectorInicio);
+		//Elimina las posiciones vacias
 		$vectorInicio = array_values( $vectorInicio );
 		if (count($vectorInicio)<= 10){
 
@@ -67,7 +71,20 @@ class Principal extends CI_Controller {
 		 $this->acomodarArray($vector4);
 		 $this->acomodarArray($vector5);
 		 
-		 $this->extraerValoresPorArray($vector1, $op1,$op2,$resul, $vecInicio);
+		 $valor_op1= $this->extraerValoresPorOperando($vector1, $op1,$vecInicio);
+		 $valor_op2= $this->extraerValoresPorOperando($vector1, $op2,$vecInicio);
+		 $valor_resul= $this->extraerValoresPorOperando($vector1, $resul,$vecInicio);
+		 echo "Op 1: ";
+		 var_dump($valor_op1);
+		 echo "<br>";
+		 echo "Op 2: ";
+		 var_dump($valor_op2);
+		 echo "<br>";
+		 echo "Resul";
+		 var_dump($valor_resul);
+		 echo "<br>";
+		 $suma = (int)$valor_op1 + (int)$valor_op2;
+		 echo "suma: ";var_dump((array_slice($valor_op1,2)));
 	}
 	//Funcion para mostrar como matriz.
 	public function acomodarArray(array $id){
@@ -81,19 +98,18 @@ class Principal extends CI_Controller {
             echo '<tr><td>' . implode('</td><td>', $o) . '</td></tr>'."<br/>";
 	        }
 	}
-	public function extraerValoresPorArray($vector, $op1, $op2, $resul,$vecInicio)	
+	public function extraerValoresPorOperando($vector, $operando, $vecInicio)	
 	{
-		$op1 = array_reverse($op1);
-		for ($i=0; $i < count($op1) ; $i++) { 
-			$pos = array_search($op1[$i], $vecInicio, true);
-			echo "vector inicio: ";
-			var_dump(count($vecInicio));
-			echo "<br>";
-			echo "pos: ";
-			var_dump($op1[$i]) ;var_dump($pos); var_dump($vecInicio[$pos]);
-			echo "<br>";
+		$operando = array_reverse($operando);
+		$op1_val = array();
+		for ($i=0; $i < count($operando) ; $i++) { 
+			$pos = array_search($operando[$i], $vecInicio, true);			
+			//extrae los valores del vector pasado como "vector" y que coincidan 
+			//con el operador 1.
+			$operando_val[] = $vector[$pos];
+			
 		}
-		
+			return $operando_val;
 	}
 
 }
